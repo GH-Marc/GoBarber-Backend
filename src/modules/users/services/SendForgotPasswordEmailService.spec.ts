9417a1f8a3075d56cdf4f3518bfecd932421dc49
Marc <marcelopgm65@gmail.com>
2020-10-05 16:18:52 -0300

    :alembic: feat: Forgot Password Service test

---
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,31 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+
+describe('SendForgorPasswordEmail', () => {
+  it('should be able to recover the password using the email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+    );
+
+    await fakeUsersRepository.create({
+      name: 'Some One',
+      email: 'someone@example.com',
+      password: '123456',
+    });
+
+    await sendForgotPasswordEmail.execute({
+      email: 'someone@example.com',
+    });
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+});
